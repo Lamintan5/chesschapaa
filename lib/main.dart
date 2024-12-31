2da2bf762f68e74f54d62627b0719edec992b515
Lamintan5 <tech@studio5ive.org>
2024-12-31 15:53:30 +0300

    start

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'package:chesschapaa/auth/login.dart';
+import 'package:chesschapaa/utils/colors.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,116 +15,101 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    final dialogBgColor = Theme.of(context).brightness == Brightness.dark
+        ? Colors.white
+        : Colors.grey[900];
+    final size = MediaQuery.of(context).size;
+    return GetMaterialApp(
+      title: "ChessChapaa",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.light(useMaterial3: true,).copyWith(
+          dialogBackgroundColor: Colors.white,
+          scaffoldBackgroundColor: Colors.white,
+          popupMenuTheme: PopupMenuThemeData(surfaceTintColor: Colors.transparent, color: Colors.white,),
+          colorScheme: ColorScheme.highContrastLight(primary: CupertinoColors.activeBlue),
+          dialogTheme: DialogTheme(surfaceTintColor: Colors.transparent,),
+          bottomSheetTheme: BottomSheetThemeData(surfaceTintColor: Colors.transparent,),
+          cardTheme: CardTheme(surfaceTintColor: Colors.transparent),
+          appBarTheme: AppBarTheme(backgroundColor: Colors.white, surfaceTintColor: Colors.transparent),
+          scrollbarTheme: ScrollbarThemeData(
+            thumbColor: MaterialStateProperty.resolveWith<Color?>(
+                  (Set<MaterialState> states) {
+                if (states.contains(MaterialState.hovered)) {
+                  return CupertinoColors.activeBlue.withOpacity(0.9); // Color when the scrollbar is hovered
+                }
+                if (states.contains(MaterialState.focused)) {
+                  return CupertinoColors.activeBlue; // Color when the scrollbar is focused
+                }
+                return CupertinoColors.activeBlue.withOpacity(0.5); // Default color
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            thickness: MaterialStateProperty.resolveWith<double?>(
+                  (Set<MaterialState> states) {
+                if (states.contains(MaterialState.hovered)) {
+                  return 7.0; // Thickness when the scrollbar is hovered
+                }
+                if (states.contains(MaterialState.dragged)) {
+                  return 7.0; // Thickness when the scrollbar is being dragged
+                }
+                return 5; // Default thickness
+              },
             ),
-          ],
+          ),
+          tabBarTheme: TabBarTheme( dividerColor: Colors.transparent,)
+      ),
+      darkTheme: ThemeData.light(useMaterial3: true,).copyWith(
+        dialogBackgroundColor: Colors.grey[900],
+        scaffoldBackgroundColor: Colors.black,
+        iconTheme: IconThemeData(color: Colors.white),
+        colorScheme: ColorScheme.highContrastDark(primary: CupertinoColors.activeBlue),
+        popupMenuTheme: PopupMenuThemeData(surfaceTintColor: Colors.transparent, color: dialogBgColor),
+        hintColor: secondaryColor,
+        textTheme: TextTheme(
+          bodyLarge: TextStyle(color: Colors.white,),
+          bodyMedium: TextStyle(color: Colors.white,),
+        ),
+        tabBarTheme: TabBarTheme(dividerColor: Colors.transparent),
+        dialogTheme: DialogTheme(surfaceTintColor: Colors.transparent,),
+        bottomSheetTheme: BottomSheetThemeData(surfaceTintColor: Colors.transparent,),
+        cardTheme: CardTheme(surfaceTintColor: Colors.transparent),
+        appBarTheme: AppBarTheme(backgroundColor: Colors.black),
+        snackBarTheme: SnackBarThemeData(
+            width: size.width > 450 ? 500: null,
+            actionTextColor: CupertinoColors.activeBlue,
+            backgroundColor: Colors.grey[900],
+            elevation: 10,
+            behavior: SnackBarBehavior.floating,
+            contentTextStyle: TextStyle(color: Colors.white),
+            closeIconColor: CupertinoColors.systemBlue,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5))
+        ),
+        scrollbarTheme: ScrollbarThemeData(
+          thumbColor: MaterialStateProperty.resolveWith<Color?>(
+                (Set<MaterialState> states) {
+              if (states.contains(MaterialState.hovered)) {
+                return CupertinoColors.activeBlue.withOpacity(0.9); // Color when the scrollbar is hovered
+              }
+              if (states.contains(MaterialState.focused)) {
+                return CupertinoColors.activeBlue; // Color when the scrollbar is focused
+              }
+              return CupertinoColors.activeBlue.withOpacity(0.5); // Default color
+            },
+          ),
+          thickness: MaterialStateProperty.resolveWith<double?>(
+                (Set<MaterialState> states) {
+              if (states.contains(MaterialState.hovered)) {
+                return 7.0; // Thickness when the scrollbar is hovered
+              }
+              if (states.contains(MaterialState.dragged)) {
+                return 7.0; // Thickness when the scrollbar is being dragged
+              }
+              return 5; // Default thickness
+            },
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: Login(),
     );
   }
 }
+
